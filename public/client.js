50c486d1b55d1961af67b66d5fc50acd63c5371d
Glitch (hello-sqlite) <none>
2019-09-30 21:11:41 +0000

    👣🍎 Checkpoint ./server.js:617503/3928 ./public/client.js:617503/8802

--- public/client.js
+++ public/client.js
@@ -1,14 +1,14 @@
 // client-side js
 // run by the browser each time your view template referencing it is loaded
 
-console.log('hello world :o');
+console.log("hello world :o");
 
 let dreams = [];
 
 // define variables that reference elements on our page
-const dreamsList = document.getElementById('dreams');
+const dreamsList = document.getElementById("dreams");
 const dreamsForm = document.forms[0];
-const dreamInput = dreamsForm.elements['dream'];
+const dreamInput = dreamsForm.elements["dream"];
 
 // a helper function to call when our request for dreams is done
 const getDreamsListener = function() {
@@ -16,23 +16,23 @@ const getDreamsListener = function() {
   dreams = JSON.parse(this.responseText);
 
   // iterate through every dream and add it to our page
-  dreams.forEach( function(row) {
+  dreams.forEach(function(row) {
     appendNewDream(row.dream);
   });
-}
+};
 
 // request the dreams from our app's sqlite database
 const dreamRequest = new XMLHttpRequest();
 dreamRequest.onload = getDreamsListener;
-dreamRequest.open('get', '/getDreams');
+dreamRequest.open("get", "/getDreams");
 dreamRequest.send();
 
 // a helper function that creates a list item for a given dream
 const appendNewDream = function(dream) {
-  const newListItem = document.createElement('li');
+  const newListItem = document.createElement("li");
   newListItem.innerHTML = dream;
   dreamsList.appendChild(newListItem);
-}
+};
 
 // listen for the form to be submitted and add a new dream when it is
 dreamsForm.onsubmit = function(event) {
@@ -43,7 +43,7 @@ dreamsForm.onsubmit = function(event) {
   dreams.push(dreamInput.value);
   appendNewDream(dreamInput.value);
 
-  // reset form 
-  dreamInput.value = '';
+  // reset form
+  dreamInput.value = "";
   dreamInput.focus();
 };
